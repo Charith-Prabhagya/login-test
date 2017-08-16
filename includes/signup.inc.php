17080f0103fd426d4e1ebd0d8ed5d0dad2404f31
charith prabhagya <cprabhagya@gmail.com>
2017-08-16 11:13:00 +0530

    Update signup.inc.php
    fill required fields

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -7,10 +7,14 @@ $last = $_POST['last'];
 $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
-
+if (empty($first) || empty($last) || empty($uid) || empty($pwd) ) {
+   echo "* One or more required fields";
+}
+else{
 $sql = "INSERT INTO user (first, last, uid, pwd) 
 VALUES ('$first', '$last', '$uid', '$pwd')"; 
 $result = $conn->query($sql);
 
-
 header("Location: ../index.php");
+}
+
